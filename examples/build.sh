b48ffc490c2c6efb828b9ea776826b978148927b
Wenzhi Liang <wenzhi.liang@gmail.com>
2017-12-20 16:20:13 +0800

    updating build script

--- examples/build.sh
+++ examples/build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-REPO=hub.c.163.com/borlandc
+REPO=hub.c.163.com/borlandc/xing
+SERVERS="server server1 evt-server strm-consumer"
+CLIENTS="client client1 notify noreply strm-producer"
 
-for s in server server1 evt-server; do
+for s in $SERVERS; do
 	GOOS=linux GOARCH=amd64 go build $s.go
+done
+for c in $CLIENTS; do
+	GOOS=linux GOARCH=amd64 go build $c.go
+done
+
+for s in $SERVERS; do
 	cat Dockerfile.server | APP=$s mo > Dockerfile
 	docker build -t $REPO/xing-$s:latest .
 	docker push $REPO/xing-$s:latest
 done
 
-for c in client client1 notify noreply; do
-	GOOS=linux GOARCH=amd64 go build $c.go
+for c in $CLIENTS; do
 	cat Dockerfile.client | APP=$c N=99999 mo > Dockerfile
 	docker build -t $REPO/xing-$c:latest .
 	docker push $REPO/xing-$c:latest
