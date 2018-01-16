84b9a4cb5d0ae55faac4cac46c93dab864f27762
Wenzhi Liang <wenzhi.liang@gmail.com>
2018-01-16 18:03:13 +0800

    ....

--- tests/xing_test.go
+++ tests/xing_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
 	"testing"
 	"time"
 
@@ -12,12 +13,47 @@ import (
 	"github.com/wzhliang/xing/examples/hello"
 )
 
+const (
+	resultTTL = time.Duration(xing.ResultQueueTTL) * time.Millisecond
+	queueTTL  = time.Duration(xing.QueueTTL) * time.Millisecond
+)
+
+var wg sync.WaitGroup
+var cancel context.CancelFunc
+
 func Test_Xing_01(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	go server(t, ctx)
-	time.Sleep(10 * time.Second)
+	t.Log("TESTING SIMPLE HAPPY CASE...")
+	time.Sleep(2 * time.Second)
 	client1(t)
+}
+
+func Test_Xing_NoCall(t *testing.T) {
+	t.Log("TESTING NO CALL + LONG WAIT + CALL...")
+	time.Sleep(2 * time.Second)
+	clientNoCall(t, resultTTL)
+}
+
+func Test_Xing_NoCallServer(t *testing.T) {
+	t.Log("TESTING NO CALL + LOOOONG WAIT + CALL...")
+	time.Sleep(2 * time.Second)
+	clientNoCall(t, queueTTL)
+}
+
+func Test_Xing_OneCall(t *testing.T) {
+	t.Log("TESTING ONE CALL + LONG WAIT + CALL...")
+	time.Sleep(2 * time.Second)
+	clientOneCall(t, resultTTL)
+}
+
+func Test_Xing_OneCallServer(t *testing.T) {
+	t.Log("TESTING ONE CALL + LOOOONG WAIT + CALL...")
+	time.Sleep(2 * time.Second)
+	clientOneCall(t, queueTTL)
+}
+
+func Test_ShutDown(t *testing.T) {
 	cancel()
+	wg.Wait()
 }
 
 func client1(t *testing.T) {
@@ -36,10 +72,84 @@ func client1(t *testing.T) {
 
 	cli := hello.NewGreeterClient("host.server", producer)
 
-	//	time.Sleep(ResultQueueTTL*time.Seconds + 10*time.Seconds)
 	ctx, cancel := context.WithTimeout(context.Background(), 5000*time.Millisecond)
 	defer cancel()
 	res, err := cli.Hello(ctx, &hello.HelloRequest{
+		Name: "王语嫣",
+	})
+	if err != nil {
+		t.Error("failed")
+	}
+	if res.Greeting != "美女好" {
+		t.Error("unexpected result")
+	}
+}
+
+func clientNoCall(t *testing.T, sleep time.Duration) {
+	url := "amqp://guest:guest@localhost:5672/"
+	mq := os.Getenv("RABBITMQ")
+	if mq == "" {
+		mq = url
+	}
+	producer, err := xing.NewClient("orchestration.controller", mq,
+		xing.SetIdentifier(&xing.RandomIdentifier{}),
+	)
+	if err != nil {
+		t.Error("failed to create new client")
+		return
+	}
+
+	cli := hello.NewGreeterClient("host.server", producer)
+
+	time.Sleep(sleep + 10*time.Second)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5000*time.Millisecond)
+	defer cancel()
+	res, err := cli.Hello(ctx, &hello.HelloRequest{
+		Name: "王语嫣",
+	})
+	if err != nil {
+		t.Error("failed")
+	}
+	if res.Greeting != "美女好" {
+		t.Error("unexpected result")
+	}
+}
+
+func clientOneCall(t *testing.T, sleep time.Duration) {
+	url := "amqp://guest:guest@localhost:5672/"
+	mq := os.Getenv("RABBITMQ")
+	if mq == "" {
+		mq = url
+	}
+	producer, err := xing.NewClient("orchestration.controller", mq,
+		xing.SetIdentifier(&xing.RandomIdentifier{}),
+	)
+	if err != nil {
+		t.Error("failed to create new client")
+		return
+	}
+
+	cli := hello.NewGreeterClient("host.server", producer)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5000*time.Millisecond)
+	defer cancel()
+	res, err := cli.Hello(ctx, &hello.HelloRequest{
+		Name: "王语嫣",
+	})
+	if err != nil {
+		t.Error("failed")
+	}
+	if res.Greeting != "美女好" {
+		t.Error("unexpected result")
+	}
+
+	t.Log("sleeping.......\n")
+	time.Sleep(sleep + 10*time.Second)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5000*time.Millisecond)
+	defer cancel()
+	res, err = cli.Hello(ctx, &hello.HelloRequest{
 		Name: "王语嫣",
 	})
 	if err != nil {
@@ -72,7 +182,8 @@ func (g *Greeter) Nihao(ctx context.Context, req *hello.HelloRequest, v *hello.V
 	return nil
 }
 
-func server(t *testing.T, ctx context.Context) {
+func server(ctx context.Context, t *testing.T) {
+	defer wg.Done()
 	mq := os.Getenv("RABBITMQ")
 	if mq == "" {
 		mq = "amqp://guest:guest@localhost:5672/"
@@ -88,7 +199,13 @@ func server(t *testing.T, ctx context.Context) {
 
 	hello.RegisterGreeterHandler(svc, &Greeter{})
 
-	fmt.Printf(" [*] Waiting for messages\n")
 	err = svc.RunWithContext(ctx)
-	log.Printf("Run returned: %v", err)
+}
+
+func init() {
+	wg = sync.WaitGroup{}
+	var ctx context.Context
+	ctx, cancel = context.WithCancel(context.Background())
+	wg.Add(1)
+	go server(ctx, nil)
 }
