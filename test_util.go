232c54eb9f9a03c3b217cd33413e5f40f59af242
Wenzhi Liang <wenzhi.liang@gmail.com>
2018-04-23 17:55:04 +0800

    removing testing imports from package xing

--- test_util.go
+++
@@ -1,9 +0,0 @@
-package xing
-
-import "testing"
-
-func _assert(t *testing.T, cond bool, msg string) {
-	if !cond {
-		t.Error(msg)
-	}
-}
--- xing_test.go
+++ xing_test.go
@@ -2,6 +2,12 @@ package xing
 
 import "testing"
 
+func _assert(t *testing.T, cond bool, msg string) {
+	if !cond {
+		t.Error(msg)
+	}
+}
+
 func Test_Xing_00(t *testing.T) {
 	T := func(cond bool, msg string) {
 		_assert(t, cond, msg)
